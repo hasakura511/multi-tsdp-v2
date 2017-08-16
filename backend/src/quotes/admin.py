8e789185984a856b1760c287b8d804c128c237fa
Lukasz Swierszcz <lukasz.swierszcz@reef.pl>
2017-08-16 16:42:07 +0200

    Quote models.

--- backend/src/quotes/admin.py
+++ backend/src/quotes/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from quotes.models import Group, Instrument, QuoteDaily
+
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ['name', 'name_plural', ]
+
+
+@admin.register(Instrument)
+class InstrumentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'symbol', 'group', 'subscribed', ]
+    list_filter = ['group', 'subscribed', ]
+
+
+@admin.register(QuoteDaily)
+class QuoteDailyAdmin(admin.ModelAdmin):
+    list_display = ['instrument', 'date', 'open', 'high', 'low', 'close', 'volume', ]
+    list_filter = ['instrument', ]
+    date_hierarchy = 'date'
--- backend/src/quotes/models.py
+++ backend/src/quotes/models.py
@@ -1,3 +1,58 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class Group(models.Model):
+    """Instrument group model."""
+
+    name = models.CharField(_('name'), max_length=63)
+    name_plural = models.CharField(_('name plural'), max_length=63, null=True, blank=True)
+
+    class Meta:
+        verbose_name = _('Group')
+        verbose_name_plural = _('Groups')
+
+    def __str__(self):
+        return self.name
+
+
+class Instrument(models.Model):
+    """Instrument model."""
+
+    name = models.CharField(_('name'), max_length=254, unique=True)
+    symbol = models.CharField(_('symbol'), max_length=32, unique=True)
+    group = models.ForeignKey(Group)
+    decimal_places = models.IntegerField(_('decimal places'), default=5)
+    subscribed = models.BooleanField(_('subscribed'), db_index=True, default=True)
+
+    class Meta:
+        verbose_name = _('Instrument')
+        verbose_name_plural = _('Instruments')
+        ordering = ('name', )
+
+    def __str__(self):
+        return self.name
+
+
+class QuoteDaily(models.Model):
+    """Quote daily model."""
+
+    id = models.BigAutoField(primary_key=True)
+
+    instrument = models.ForeignKey(Instrument)
+    date = models.DateField(_('date'))
+
+    open = models.DecimalField(_('open'), max_digits=10, decimal_places=5, default=0)
+    high = models.DecimalField(_('high'), max_digits=10, decimal_places=5, default=0)
+    low = models.DecimalField(_('low'), max_digits=10, decimal_places=5, default=0)
+    close = models.DecimalField(_('close'), max_digits=10, decimal_places=5, default=0)
+    volume = models.BigIntegerField(_('volume'), default=0)
+
+    class Meta:
+        verbose_name = _('Quote daily')
+        verbose_name_plural = _('Quotes daily')
+
+        unique_together = ('instrument', 'date')
+
+    def __str__(self):
+        return '{} - {}'.format(self.instrument, self.date)
