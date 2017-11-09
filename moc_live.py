1d3c533c34f4c8450bf09cf965e15adae4c90226
Hidemi Asakura <hidemi@gmail.com>
2017-11-09 11:05:20 +0800

    dat

--- moc_live.py
+++ moc_live.py
@@ -844,18 +844,18 @@ def get_timetable(contractsDF):
             timetable=pd.DataFrame(thDict,index=[sym+' open',sym+' close',sym+' trigger'])
         else:
             timetable=pd.concat([timetable, pd.DataFrame(thDict,index=[sym+' open',sym+' close',sym+' trigger'])], axis=0)
-    
+    print 'csidate', csidate
     filedates=[str(x) for x in [d for d in timetable.columns.astype(int) if d>csidate]]
     filedate=filedates[0]
     print filedates, filedate
-    print timetable
+    #print timetable.to_csv()
     #fill nans with the next filedate
-    if len(filedates)>1:
-        next_filedate=filedates[1]
-        timetable[filedate]=[timetable[next_filedate].ix[idx] if timetable[filedate].ix[idx] is np.nan \
-                                        else timetable[filedate].ix[idx] for idx in timetable[filedate].index]
-        timetable=timetable.drop([next_filedate],axis=1)
-    print timetable                                  
+    #if len(filedates)>1:
+    #    next_filedate=filedates[1]
+    #    timetable[filedate]=[timetable[next_filedate].ix[idx] if timetable[filedate].ix[idx] is np.nan \
+    #                                    else timetable[filedate].ix[idx] for idx in timetable[filedate].index]
+    #    timetable=timetable.drop([next_filedate],axis=1)
+    print timetable.to_csv()                                  
     filename=timetablePath+str(filedate)+'.csv'
     timetable.to_csv(filename, index=True)
     print 'saved', filename
